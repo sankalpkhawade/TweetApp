db86b9f1db5ad14d08a48743bf1658293078f398
Lakshmini <lakshminikothapalli56@gmail.com>
2021-04-25 20:22:01 +0530

    Feat: Comment hardcoded

--- src/components/custom-nav-bar/custom-nav-bar.helper.js
+++ src/components/custom-nav-bar/custom-nav-bar.helper.js
@@ -1,29 +1,29 @@
 import { HttpGet } from "../../services/api-services";
 import { BASE_URI, GET_USER } from "../../constants/endpoints";
 
-export const fetchLoggedInUserDetails = () => {
+export const fetchLoggedInUserDetails = async () => {
   try {
       let credentials = "Basic " + localStorage.getItem("token");
       let apiUrl = BASE_URI + GET_USER + localStorage.getItem("loginId");
       let headers = {
           "Authorization": credentials
       }
-      // let response = await HttpGet(apiUrl, {}, headers)
-      let response = {
-          data: {
-              "usersDto": [
-                  {
-                      "loginId": "sainag98",
-                      "firstName": "Sainag",
-                      "lastName": "Chunduru",
-                      "emailId": "sainagchunduru23@gmail.com",
-                      "password": "Dsf",
-                      "contactNumber": 9098765432,
-                      "loggedIn": null
-                  }
-              ]
-          }
-      }
+      let response = await HttpGet(apiUrl, {}, headers)
+    //   let response = {
+    //       data: {
+    //           "usersDto": [
+    //               {
+    //                   "loginId": "sainag98",
+    //                   "firstName": "Sainag",
+    //                   "lastName": "Chunduru",
+    //                   "emailId": "sainagchunduru23@gmail.com",
+    //                   "password": "Dsf",
+    //                   "contactNumber": 9098765432,
+    //                   "loggedIn": null
+    //               }
+    //           ]
+    //       }
+    //   }
       return response.data.usersDto[0];
   } catch (e) {
       throw e;
