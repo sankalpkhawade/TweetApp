643bcec1245cf584503a18a5a3f1fe5617bc8e8d
Lakshmini <lakshminikothapalli56@gmail.com>
2021-04-25 23:55:36 +0530

    Fix: Forgot password error fx

--- src/pages/forgot-passwd/forgot-passwd.helper.js
+++ src/pages/forgot-passwd/forgot-passwd.helper.js
@@ -4,12 +4,15 @@ import { BASE_URI, FORGOT_PASSWD } from "../../constants/endpoints";
 export const forgotPassword = async (email, password) => {
     try {
         let apiUrl = BASE_URI + FORGOT_PASSWD;
-        await HttpPost(apiUrl, {
+        let response = await HttpPost(apiUrl, {
             userDto: {
                 loginId: email,
                 password: password
             }
         })
+        if(response.data.statusMessage == "FAILURE"){
+            throw "User does not exist"
+        }
     } catch (e) {
         throw e;
     }
